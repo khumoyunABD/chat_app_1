771a44616feb0ce2cd6437e31d95d3868b3cef7b
khumoyunABD <allhater37@gmail.com>
2024-02-19 15:18:35 +0500

    adding_automated_cloud_function

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,6 +1,7 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,22 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+
+    fcm.subscribeToTopic('group_chat');
+
+    // final token = await fcm.getToken();
+    // print(token);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
+
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser!;
--- lib/screens/cool_animation.dart
+++ lib/screens/cool_animation.dart
@@ -1,37 +1,36 @@
-import 'package:flip_carousel/flip_carousel.dart';
-import 'package:flutter/material.dart';
+// import 'package:flip_carousel/flip_carousel.dart';
+// import 'package:flutter/material.dart';
 
-class CoolAnimation extends StatelessWidget {
-  const CoolAnimation({super.key});
+// class CoolAnimation extends StatelessWidget {
+//   const CoolAnimation({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    List<dynamic> cardItems = [
-      'https://i.ibb.co/QYdQHBw/batman2.jpg',
-      'https://i.ibb.co/vwhykYp/spider1.jpg',
-      'https://i.ibb.co/H4VrQS4/spider2.jpg',
-      'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/733b5da9-efe6-4697-a4f9-9e9d4975f828/de8g75a-a128f1ac-ec63-4485-b913-3b0f00fb9b63.jpg/v1/fill/w_730,h_1095,q_70,strp/madara_uchiha__by_sasukegfx_de8g75a-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDA5NiIsInBhdGgiOiJcL2ZcLzczM2I1ZGE5LWVmZTYtNDY5Ny1hNGY5LTllOWQ0OTc1ZjgyOFwvZGU4Zzc1YS1hMTI4ZjFhYy1lYzYzLTQ0ODUtYjkxMy0zYjBmMDBmYjliNjMuanBnIiwid2lkdGgiOiI8PTI3MzAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.maUBrrI4nlouojnf2IhpGFbcUpz9meNgsAGX92RZYI8',
-      //Container(color: Colors.amber, child: const FlutterLogo()),
-    ];
+//   @override
+//   Widget build(BuildContext context) {
+//     List<dynamic> cardItems = [
+//       'https://i.ibb.co/QYdQHBw/batman2.jpg',
+//       'https://i.ibb.co/vwhykYp/spider1.jpg',
+//       'https://i.ibb.co/H4VrQS4/spider2.jpg',
+//       'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/733b5da9-efe6-4697-a4f9-9e9d4975f828/de8g75a-a128f1ac-ec63-4485-b913-3b0f00fb9b63.jpg/v1/fill/w_730,h_1095,q_70,strp/madara_uchiha__by_sasukegfx_de8g75a-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDA5NiIsInBhdGgiOiJcL2ZcLzczM2I1ZGE5LWVmZTYtNDY5Ny1hNGY5LTllOWQ0OTc1ZjgyOFwvZGU4Zzc1YS1hMTI4ZjFhYy1lYzYzLTQ0ODUtYjkxMy0zYjBmMDBmYjliNjMuanBnIiwid2lkdGgiOiI8PTI3MzAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.maUBrrI4nlouojnf2IhpGFbcUpz9meNgsAGX92RZYI8',
+//     ];
 
-    return Center(
-      child: FlipCarousel(
-        items: cardItems,
-        transitionDuration: const Duration(milliseconds: 400),
-        isAssetImage: false,
-        border: Border.all(width: 5, color: const Color(0xFFFFFFFF)),
-        width: 250,
-        height: 400,
-        fit: BoxFit.cover,
-        perspectiveFactor: 0.002,
-        layersGap: 30,
-        onChange: (int pageIndex) {
-          print(pageIndex);
-        },
-        onTap: () {
-          print("tap");
-        },
-      ),
-    );
-  }
-}
+//     return Center(
+//       child: FlipCarousel(
+//         items: cardItems,
+//         transitionDuration: const Duration(milliseconds: 400),
+//         isAssetImage: false,
+//         border: Border.all(width: 5, color: const Color(0xFFFFFFFF)),
+//         width: 250,
+//         height: 400,
+//         fit: BoxFit.cover,
+//         perspectiveFactor: 0.002,
+//         layersGap: 30,
+//         onChange: (int pageIndex) {
+//           print(pageIndex);
+//         },
+//         onTap: () {
+//           print("tap");
+//         },
+//       ),
+//     );
+//   }
+// }
